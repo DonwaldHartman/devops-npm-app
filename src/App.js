391339a7e8c44e64091b56732a8f71daeb382072
Donwald Hartman <donwald.hartman@gmail.com>
2023-11-28 12:01:09 +0000

    Updating

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useCallback } from 'react';
 import Particles from "react-particles";
 import { loadFull } from "tsparticles";
-import devops_logo from './assets/devops-shield.png';
+import devops_logo from './assets/wizard.png';
 import './App.css';
 import particlesOptions from "./particles.json";
 import 'reactjs-popup/dist/index.css';
@@ -20,7 +20,7 @@ function App() {
                 <p className='Hero'><img src={devops_logo} className="" alt="logo"/></p>
 <h3> This is a sample project for Kubernetes Nodejs testing on DevOps projects</h3>
   <hr></hr>
-  <h3>If you are able to read this then this app is clearly working </h3>
+  <h3>Hey Donwald! If you are able to read this then this app is clearly working </h3>
             </header>
             </div>
         </div>
